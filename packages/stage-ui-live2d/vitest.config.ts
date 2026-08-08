afdf05d180794b3b02d9e0b7c1081055a64bff03
Yangyi Liu <55562961+starryark@users.noreply.github.com>
2026-08-07 19:52:33 -0700

    chore(live2d): avoid package-local plugin dependency

--- packages/stage-ui-live2d/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { Cubism2Core } from '@proj-airi/unplugin-live2d-sdk/vite'
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-  plugins: [Cubism2Core()],
-  test: {
-    include: ['src/**/*.test.ts'],
-  },
-})
